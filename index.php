d270f6ba838f2318776fdabd0943911ff2c4917d
Dieter De Weirdt <dieter.deweirdt@arteveldehs.be>
2023-11-20 21:06:48 +0100

    add first page

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CMS</title>
+</head>
+<body>
+    <h1>Hello PGM!</h1>
+
+    <ul>
+        <li><a href="#">OOP in PHP</a></li>
+        <li><a href="#">MVC</a></li>
+        <li><a href="#">Laravel</a></li>
+    </ul>
+</body>
+</html>
