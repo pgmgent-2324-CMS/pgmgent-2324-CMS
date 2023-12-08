3f0c345249d1199e6b12bda126dac721f0a9f763
Dieter De Weirdt <dieter.deweirdt@arteveldehs.be>
2023-12-08 09:27:27 +0100

    Add course and factory

---
+++ Laravel/app/Models/Course.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Course extends Model  {
+
+    use HasFactory;
+
+    public function teachers(): BelongsToMany
+    {
+        return $this->belongsToMany(Teacher::class);
+    }
+
+    protected $fillable = [
+        'name',
+    ];
+    
+}
